e2e7c42b38530ff5e2e26ee522c1cc12e9d5a24e
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2022-11-21 12:30:56 +0000

    added notes for how operating sytems coursework should go

--- user/memory_management.c
+++ user/memory_management.c
@@ -4,6 +4,9 @@
 #include "kernel/param.h"
 #include <stddef.h>
 
+
+//Create a linked list that acts as a page table and data
+//Linked list allocates pages and connects them to the pointer that begins that page
 void* _malloc(int size) {
     void *allocated_mem;
 
@@ -11,7 +14,7 @@ void* _malloc(int size) {
         allocated_mem = 0;
     }
     else {
-        char *memory = sbrk(size); //Grow the size of the heap
+        char *memory = sbrk(size); //don't need this
         allocated_mem = (void*)memory;
     }
     return allocated_mem;
