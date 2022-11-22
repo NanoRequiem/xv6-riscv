ee7658adef6fcd5cb612aecefef13ed898543164
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2022-11-22 13:27:51 +0000

    Updated malloc to search through pre-existing page table before expanding it

--- user/memory_management.c
+++ user/memory_management.c
@@ -7,6 +7,7 @@
 //Struct to store page table and table values
 struct page {
     void *data_ptr; //pointer to start of data value that has been allocated
+    bool allocation; //boolean to check if a page is currently in use
     struct page *next; //Next pointer within the list
 };
 
@@ -23,21 +24,40 @@ void* _malloc(int size) {
         return zero_size;
     }
     
+    //Search through the current page table to see if we have a page that is currently free
+    struct page *search = header;
+    while(search != NULL) {
+        //If we have a page that is currently free then return that
+        if(search->allocation) {
+            return search->data_ptr
+        }
+
+        search = search->next;
+    }
+
     //We know that the size is greater than 0 so now we can move onto allocating memory
-    struct page *allocated = (struct page *) malloc(sizeof(struct page));
+    struct page *newAllocate = (struct page *) malloc(sizeof(struct page));
 
     //Grow memeory by what we need and save pointer to page table
-    allocated->data_ptr = sbrk(size); 
-    
+    newallocate->data_ptr = sbrk(size);
+    //Since we have allocated memory set allocation as true 
+    newAllocate->allocation = true;
+
     //Move header and next so that we can properly use the linked list
-    allocated->next = header;
-    header = allocated;
+    newAllocate->next = header;
+    header = newAllocate;
 
     //Return the saved pointer
-    return allocated->data_ptr;
+    return newAllocate->data_ptr;
 }
 
 //search through linked list until NULL is seen
 void _free(void *ptr) {
-    
+    struct page *search = header;
+
+    while(search != NULL) {
+        if(search->data_ptr == ptr) {
+            
+        }
+    }
 }
