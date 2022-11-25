7dc976b9e867555164a4e772276a83d409abdb55
Brooklyn Mcswiney <117984685+NanoRequiem@users.noreply.github.com>
2022-11-25 16:55:58 +0000

    Actually made a test

--- user/test.c
+++ user/test.c
@@ -1,6 +1,7 @@
 #include "user/memory_management.h"
 #include "kernel/types.h"
 #include "user/user.h"
+#include <stddef.h>
 
 int main(int argc, char *argv[]) {
     printf("-----STARTING TEST-----\n");
@@ -9,12 +10,30 @@ int main(int argc, char *argv[]) {
 
     printf("%d\n", x / y + (x % y !=0));
 
-    // printf("Testing NULL: ");
-    // int *bruh = _malloc(0);
+    printf("Testing NULL: ");
+    void *bruh = _malloc(0);
 
-    // if(bruh == 0) {
-    //     printf("passed \n");
-    // }
+    if(bruh == NULL) {
+         printf("passed \n");
+    }
+
+    printf("Basic allocation: ");
+    void *allocation = _malloc(4);
+    void *allocatedeez = _malloc(12);
+    void *ally = _malloc(9);
+
+    if(allocation != NULL) {
+        printf("passed \n");
+    }
+
+    printf("Free then allocate");
+
+    _free(allocatedeez);
+    void *freeallocate = _malloc(4);
+    printf("free'd allocation = %x\n", allocatedeez);
+    printf("new allocation = %x\n", freeallocate);
+    printf("ally = %x\n", ally);
+    
 
     return 0;
 }
